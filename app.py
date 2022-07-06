be0b8bbde03af0754199b158a49c0f02079392de
Dean Keinan <code@keinan.xyz>
2022-07-06 12:13:57 -0400

    add redis_fwc, requestrate

--- app.py
+++ app.py
@@ -23,15 +23,20 @@ from storage import (
     LocalFixedWindowCounter,
     LocalLeakyBucket,
     LocalSlidingLog,
+    LocalSlidingWindowCounter,
     LocalTokenBucket,
+    RedisFixedWindowCounter,
+    RequestRate,
 )
 
 
 def identify_by_client_ip():
     return request.remote_addr
 
+
 def identify_by_api_key():
-    return request.headers['api-key']
+    return request.headers["api-key"]
+
 
 app = Flask(__name__)
 
@@ -40,7 +45,8 @@ local_tb = LocalTokenBucket()
 local_lb = LocalLeakyBucket()
 local_fw = LocalFixedWindowCounter()
 local_sl = LocalSlidingLog()
-
+local_swc = LocalSlidingWindowCounter()
+redis_fwc = RedisFixedWindowCounter('test_fwc')
 # Intended rate limit for all limiters is 6 reqs/min
 @app.route("/")
 def hello_world():
@@ -56,6 +62,8 @@ def hello_world():
                     <li><a href="/local/lb">Local_LeakyBucket</a></li>
                     <li><a href="/local/fw">Local_FixedWindowCount</a></li>
                     <li><a href="/local/swl">Local_SlidingWindowLog</a></li>
+                    <li><a href="/local/swc">Local_SlidingWindowCounter</a></li>
+                    <li><a href="/redis/fw">Redis_FixedWindowCount</a></li>
                 </ul>
             </body>
 
@@ -64,25 +72,44 @@ def hello_world():
     )
 
 
+SIX_PER_MIN = RequestRate(6, timedelta(minutes=1))
+
+
 @app.route("/local/tb")
-@local_tb(identifier_func=identify_by_api_key, rate=0.1, capacity=6)
+@local_tb(
+    identifier_func=identify_by_client_ip, refill_rate=SIX_PER_MIN, token_capacity=6
+)
 def hello_local_tb():
     return "Hello! I'm rate limited by an in-memory TokenBucket algo"
 
 
 @app.route("/local/lb")
-@local_lb(identifier_func=identify_by_api_key, rate=0.1, capacity=6)
+@local_lb(
+    identifier_func=identify_by_client_ip, leak_rate=SIX_PER_MIN, token_capacity=6
+)
 def hello_local_lb():
     return "Hello! I'm rate limited by an in-memory LeakyBucket algo"
 
 
 @app.route("/local/fw")
-@local_fw(identifier_func=identify_by_api_key, rate=6)
+@local_fw(identifier_func=identify_by_client_ip, rate=SIX_PER_MIN)
 def hello_local_fw():
     return "Hello! I'm rate limited by an in-memory FixedWindowCounter algo"
 
+@app.route("/redis/fw")
+@redis_fwc(identifier_func=identify_by_client_ip, rate=SIX_PER_MIN)
+def hello_redis_fwc():
+    return "Hello! I'm rate limited by a redis based Fixed Window Counter algo"
 
 @app.route("/local/swl")
-@local_sl(identifier_func=identify_by_api_key, rate=6)
+@local_sl(identifier_func=identify_by_client_ip, rate=SIX_PER_MIN)
 def hello_local_sl():
     return "Hello! I'm rate limited by an in-memory Sliding Window Log algo"
+
+
+@app.route("/local/swc")
+@local_swc(identifier_func=identify_by_client_ip, rate=SIX_PER_MIN)
+def hello_local_swc():
+    return "Hello! I'm rate limited by an in-memory Sliding Window Counter algo"
+
+
--- storage.py
+++ storage.py
@@ -1,33 +1,53 @@
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from functools import wraps
 import math
 import threading
 from collections import Counter, defaultdict
-from typing import Callable, DefaultDict, Dict
+from typing import Callable, DefaultDict, Dict, Tuple
 from flask import Response
 from rich import inspect
 from functools import wraps
-import abc
 import time
 from rich import inspect
+import redis
+from redis.exceptions import ConnectionError
+
+redis_client = redis.Redis('localhost',port=6379)
+
+
+def floor_dt(dt, delta):
+    rounded = dt - (dt - datetime.min) % delta
+    return rounded
+
+@dataclass
+class RequestRate:
+    requests: int
+    period: timedelta
+
+    @property
+    def per_second(self):
+        return self.requests / self.period.total_seconds()
+
 
 """
 Data structures & classes for various rate limiting algorithms with 
 corresponding storage backends.
 """
 
-
 class LocalTokenBucket:
     """
-    A token bucket is a container that has pre-defined capacity, and is refilled at a preset rate.
-    Once the bucket is full, additional tokens overflow.
+    A token bucket is a container that has pre-defined capacity, and is refilled at a preset rate.Once the bucket is full, additional tokens overflow.
     """
 
     def __init__(self):
         self.buckets = {}
         self.lock = threading.Lock()
 
-    def refill(self, key, rate, capacity):
+    def refill(self, key: str, refill_rate: RequestRate, token_capacity):
+        """
+        Perform a 'refill' at request time based on elapsed time * rate
+        """
         with self.lock:
             try:
                 current_tokens, last_refill = self.buckets[key]
@@ -38,35 +58,44 @@ class LocalTokenBucket:
                 if now < last_refill:
                     return
 
-                additional_tokens = rate * (now - last_refill)
+                additional_tokens = refill_rate.per_second * (now - last_refill)
                 self.buckets[key] = [
-                    min(capacity, current_tokens + additional_tokens),
+                    min(token_capacity, current_tokens + additional_tokens),
                     now,
                 ]
             except KeyError:
-                self.buckets[key] = [capacity, time.monotonic()]
+                self.buckets[key] = [token_capacity, time.monotonic()]
 
-    def consume(self, key, cost=1):
+    def consume(self, key, token_cost) -> Tuple[bool, int, datetime]:
+        """
+        Try to consume a cost from a token bucket
+        """
         with self.lock:
             current_tokens, last_refill = self.buckets[key]
-            if current_tokens < cost:
+            if current_tokens < token_cost:
                 return False, current_tokens, last_refill
-            self.buckets[key][0] -= cost
-            return True, current_tokens - cost, last_refill
+            self.buckets[key][0] -= token_cost
+            return True, current_tokens - token_cost, last_refill
 
     def inspect(self):
         inspect(self.buckets)
 
-    def __call__(self, identifier_func: Callable, rate=1, capacity=6, cost=1):
+    def __call__(
+        self,
+        identifier_func: Callable,
+        refill_rate: RequestRate,
+        token_capacity=6,
+        token_cost=1,
+    ):
         """
         Decorator that limits usage of the decorated function using a Token Bucket.
 
         Args:
             backend (TokenBucket): a TokenBucket storage backend
             identifier_func (Callable): some function that discriminates each request
-            rate (float): refill rate in tokens per second
-            capacity (int): maximum token amount per identifier
-            cost (int): the token cost of the decorated function
+            refill_rate (float): refill rate in tokens per second
+            token_capacity (int): maximum token amount per identifier
+            token_cost (int): the token cost of the decorated function
 
         Returns:
             A 429 message if the rate limit is exceeded, the result of the decorated function otherwise.
@@ -76,11 +105,11 @@ class LocalTokenBucket:
             @wraps(f)
             def wrapper(*args, **kwargs):
                 key = identifier_func()
-                self.refill(key, rate, capacity)
-                allow, tokens, last_refill = self.consume(key, cost)
+                self.refill(key, refill_rate, token_capacity)
+                allow, tokens, last_refill = self.consume(key, token_cost)
                 rate_headers = {
-                    "api-ratelimit-limit": rate,
-                    "api-ratelimit-remaining": tokens / cost,
+                    "api-ratelimit-limit": refill_rate.per_second,
+                    "api-ratelimit-remaining": tokens / token_cost,
                 }
 
                 if allow:
@@ -110,15 +139,15 @@ class LocalLeakyBucket:
         self.buckets = {}
         self.lock = threading.Lock()
 
-    def pour(self, key, capacity, cost=1):
+    def pour(self, key, token_capacity, token_cost):
         with self.lock:
             current_tokens, last_leak = self.buckets[key]
-            if current_tokens + cost > capacity:
+            if current_tokens + token_cost > token_capacity:
                 return False, current_tokens, last_leak
-            self.buckets[key][0] += cost
-            return True, current_tokens + cost, last_leak
+            self.buckets[key][0] += token_cost
+            return True, current_tokens + token_cost, last_leak
 
-    def leak(self, key, rate):
+    def leak(self, key, leak_rate: RequestRate):
         with self.lock:
             try:
                 current_tokens, last_leak = self.buckets[key]
@@ -129,12 +158,18 @@ class LocalLeakyBucket:
                 if now < last_leak:
                     return
 
-                reduction = rate * (now - last_leak)
+                reduction = leak_rate.per_second * (now - last_leak)
                 self.buckets[key] = [max(0, current_tokens - reduction), now]
             except KeyError:
                 self.buckets[key] = [0, time.monotonic()]
 
-    def __call__(self, identifier_func: Callable, rate=1, capacity=6, cost=1):
+    def __call__(
+        self,
+        identifier_func: Callable,
+        leak_rate: RequestRate,
+        token_capacity=6,
+        token_cost=1,
+    ):
         """
         Decorator that limits usage of the decorated function using a Leaky Bucket (as a meter).
 
@@ -153,12 +188,11 @@ class LocalLeakyBucket:
             @wraps(f)
             def wrapper(*args, **kwargs):
                 key = identifier_func()
-                self.leak(key, rate)
-                allow, tokens, last_leak = self.pour(key, capacity, cost)
-                print(allow, tokens, last_leak)
+                self.leak(key, leak_rate)
+                allow, tokens, last_leak = self.pour(key, token_capacity, token_cost)
                 rate_headers = {
-                    "api-ratelimit-limit": rate,
-                    "api-ratelimit-remaining": (capacity - tokens) / cost,
+                    "api-ratelimit-limit": leak_rate.per_second,
+                    "api-ratelimit-remaining": (token_capacity - tokens) / token_cost,
                 }
 
                 if allow:
@@ -181,31 +215,30 @@ class LocalSlidingLog:
         self.logs = {}
         self.lock = threading.Lock()
 
-    def consume(self, key, rate):
+    def consume(self, key, rate: RequestRate):
         with self.lock:
             now = datetime.now()
             # TODO: use some sorted, thread safe data structure for this
-            lower_bound = now - timedelta(minutes=1)
+            lower_bound = now - rate.period
             try:
                 log = self.logs[key]
                 log.append(now)
                 log = [*filter(lambda ts: ts > lower_bound, log)]
                 size = len(log)
-                if size > rate:
-                    return False, rate - size, log[-1] + timedelta(minutes=1)
-                return True, rate - size, log[-1] + timedelta(minutes=1)
+                if size > rate.requests:
+                    return False, rate.requests - size, log[-1] + rate.period
+                return True, rate.requests - size, log[-1] + rate.period
             except KeyError:
                 self.logs[key] = [now]
-                return True, rate - 1, now + timedelta(minutes=1)
+                return True, rate.requests - 1, now + rate.period
 
-    def __call__(self, identifier_func: Callable, rate=2):
+    def __call__(self, identifier_func: Callable, rate: RequestRate):
         """
         Decorator that limits usage of the decorated function using a Sliding Window Log.
 
         Args:
-            backend (SlidingWindow): a Slidingwindow storage backend
             identifier_func (Callable): some function that discriminates each request
-            rate (float): refill rate in tokens per second
+            rate (RequestRate): rate limit
 
         Returns:
             A 429 message if the rate limit is exceeded, the result of the decorated function otherwise.
@@ -217,7 +250,7 @@ class LocalSlidingLog:
                 key = identifier_func()
                 allow, remaining, next_reset = self.consume(key, rate)
                 rate_headers = {
-                    "api-ratelimit-limit": rate,
+                    "api-ratelimit-limit": rate.per_second,
                     "api-ratelimit-remaining": remaining,
                     "api-ratelimit-reset": next_reset,
                 }
@@ -246,24 +279,24 @@ class LocalFixedWindowCounter:
         self.counter = {}
         self.lock = threading.Lock()
 
-    def consume(self, key, rate):
+    def consume(self, key, rate: RequestRate):
         with self.lock:
             now = datetime.now()
-            latest_window = now.replace(second=0, microsecond=0, minute=now.minute)
+            latest_window = floor_dt(now, rate.period)
             try:
                 count, active_window = self.counter[key]
                 if active_window < latest_window:
                     self.counter[key] = [0, latest_window]
-                    return True, rate, latest_window + timedelta(minutes=1)
-                if count < rate:
+                    return True, rate.requests, latest_window + rate.period
+                if count + 1 < rate.requests:
                     self.counter[key][0] += 1
-                    return True, rate - count - 1, active_window + timedelta(minutes=1)
-                return False, 0, active_window + timedelta(minutes=1)
+                    return True, rate.requests - count - 1, active_window + rate.period
+                return False, 0, active_window + rate.period
             except KeyError:
                 self.counter[key] = [0, latest_window]
-                return True, rate, latest_window + timedelta(minutes=1)
+                return True, rate.requests, latest_window + rate.period
 
-    def __call__(self, identifier_func: Callable, rate=2):
+    def __call__(self, identifier_func: Callable, rate: RequestRate):
         """
         Decorator that limits usage of the decorated function using a Fixed Window.
 
@@ -282,7 +315,73 @@ class LocalFixedWindowCounter:
                 key = identifier_func()
                 allow, remaining, next_reset = self.consume(key, rate)
                 rate_headers = {
-                    "api-ratelimit-limit": rate,
+                    "api-ratelimit-limit": rate.per_second,
+                    "api-ratelimit-remaining": remaining,
+                    "api-ratelimit-reset": next_reset,
+                }
+                if allow:
+                    resp = f(*args, **kwargs)
+                    return Response(response=resp, status=200, headers=rate_headers)
+                else:
+                    return Response(
+                        response=f"Rate Limit for {key} exceeded.",
+                        status=429,
+                        headers=rate_headers,
+                    )
+
+            return wrapper
+
+        return decorator
+
+class RedisFixedWindowCounter:
+    """
+    A fixed window counter maps an identifier to a (count, timestamp) tuple.
+    Rate limits are imposed by a maximum rate for each fixed timestamp 'window'.
+    """
+
+    def __init__(self,keyspace):
+        self.hash_name = keyspace
+
+    def consume(self, key, rate: RequestRate):
+
+        now = datetime.now()
+        latest_window = floor_dt(now, rate.period)
+        if redis_client.hexists(self.hash_name,key):
+            val = redis_client.hget(self.hash_name,key).decode()
+            count, active_window = val.split('##')
+            active_window = datetime.fromtimestamp(int(float(active_window)))
+            count = int(count)
+            if active_window < latest_window:
+                redis_client.hset(self.hash_name,key,f"0##{latest_window.timestamp()}")
+                return True, rate.requests, latest_window + rate.period
+            if count + 1 < rate.requests:
+                redis_client.hset(self.hash_name,key,f"{count+1}##{latest_window.timestamp()}")
+                return True, rate.requests - count - 1, active_window + rate.period
+            return False, 0, active_window + rate.period
+        else:
+            redis_client.hset(self.hash_name,key,f"0##{latest_window.timestamp()}")
+            return True, rate.requests, latest_window + rate.period
+
+    def __call__(self, identifier_func: Callable, rate: RequestRate):
+        """
+        Decorator that limits usage of the decorated function using a Fixed Window.
+
+        Args:
+            backend (WindowCounter): a FixedWindow storage backend
+            identifier_func (Callable): some function that discriminates each request
+            rate (float): refill rate in tokens per second
+
+        Returns:
+            A 429 message if the rate limit is exceeded, the result of the decorated function otherwise.
+        """
+
+        def decorator(f):
+            @wraps(f)
+            def wrapper(*args, **kwargs):
+                key = identifier_func()
+                allow, remaining, next_reset = self.consume(key, rate)
+                rate_headers = {
+                    "api-ratelimit-limit": rate.per_second,
                     "api-ratelimit-remaining": remaining,
                     "api-ratelimit-reset": next_reset,
                 }
@@ -301,7 +400,6 @@ class LocalFixedWindowCounter:
         return decorator
 
 
-
 class LocalSlidingWindowCounter:
     """
     A fixed window counter maps an identifier to a (count, timestamp) tuple.
@@ -309,34 +407,45 @@ class LocalSlidingWindowCounter:
     """
 
     def __init__(self):
-        self.counters:Dict[str,DefaultDict] = {}
+        self.counters: Dict[str, DefaultDict] = {}
         self.lock = threading.Lock()
 
-    def consume(self, key, rate):
+    def consume(self, key, rate: RequestRate):
         with self.lock:
             now = datetime.now()
-            now_minute_floor = now.replace(microsecond=0,second=0)
-            rolling_lower_bound= now - timedelta(minutes=1)
-            sub_window = now.replace(microsecond=0)
-            #todo: this feels clunky
+            now_minute_floor = floor_dt(now, rate.period)
+            rolling_lower_bound = now - rate.period
+            frame = floor_dt(now, timedelta(seconds=1))
+            # todo: this feels clunky
             try:
-                _windows = self.counters[key]
-                windows = {k:v for k,v in _windows.items() if k < rolling_lower_bound}
-                self.counters[key] = windows
-                windows[sub_window] +=1
-                prior_sum = sum(windows[k] for k in windows.keys() if k < now_minute_floor)
-                current_sum = sum(windows[k] for k in windows.keys() if k > now_minute_floor)
-                weighted_sum = math.floor(prior_sum * (1-(now.minute/60)) + current_sum)
-                if weighted_sum < rate:
-                    return True, rate - weighted_sum, None
+                windows = self.counters[key]
+                pruned_windows = defaultdict(int)
+                prior_sum = 0
+                current_sum = 0
+                for k, v in windows.items():
+                    if k > rolling_lower_bound:
+                        pruned_windows[k] = v
+                        if k < now_minute_floor:
+                            prior_sum += v
+                        else:
+                            current_sum += v
+                self.counters[key] = pruned_windows
+                weighted_sum = math.floor(
+                    prior_sum * (1 - (now.second / 60)) + current_sum
+                )
+                self.counters[key][frame] += 1
+                if weighted_sum < rate.requests:
+                    return True, rate.requests - weighted_sum, None
                 else:
-                    return False, rate - weighted_sum, None
+                    return False, rate.requests - weighted_sum, None
             except KeyError:
                 self.counters[key] = defaultdict(int)
-                self.counters[key][sub_window] +=1
-                return True, rate, None
+                windows = self.counters[key]
+                self.counters[key][frame] += 1
 
-    def __call__(self, identifier_func: Callable, rate=2):
+                return True, rate.requests - 1, None
+
+    def __call__(self, identifier_func: Callable, rate: RequestRate):
         """
         Decorator that limits usage of the decorated function using a Fixed Window.
 
@@ -355,7 +464,7 @@ class LocalSlidingWindowCounter:
                 key = identifier_func()
                 allow, remaining, _ = self.consume(key, rate)
                 rate_headers = {
-                    "api-ratelimit-limit": rate,
+                    "api-ratelimit-limit": rate.per_second,
                     "api-ratelimit-remaining": remaining,
                 }
                 if allow:
